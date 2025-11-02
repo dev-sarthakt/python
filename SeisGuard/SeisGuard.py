8eec5c210b976dc5e57214f9449b41505235ff61
Thange Sarthak Somnath <sarthakthange1904@gmail.com>
2025-11-02 18:38:56 +0530

    Integrate pygame for sound alerts and modify data fetch
    Added pygame for sound notifications and updated earthquake data handling.

--- SeisGuard/SeisGuard.py
+++ SeisGuard/SeisGuard.py
@@ -1,14 +1,26 @@
-import sys, time, json, requests
+import sys, time, json, requests, pygame, math
 from tkinter import *
 from tkinter import ttk, messagebox
 
 update = 60
 progress = 0
+max_mag = 0
+max_mag_loc = ''
+eq_coo = []
 url = 'https://ipinfo.io/json'
+
+def eas():
+    pygame.mixer.init()
+    pygame.mixer.music.load('eas.ogg')
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        time.sleep(1)
+    
+    eas()
 
 def usgs():
     current_time = time.localtime()
-    past_time = time.localtime(time.time()-60)
+    past_time = time.localtime(time.time()-(60*5))
 
     present = time.strftime("%Y-%m-%dT%H:%M:%S", current_time)
     past = time.strftime("%Y-%m-%dT%H:%M:%S", past_time)
@@ -19,6 +31,32 @@ def usgs():
     if usgs_request.status_code == 200:
         with open('usgs.json', 'w') as f:
             json.dump(usgs_request_json, f, indent=4)
+        with open('usgs.json', 'r') as f:
+            data = json.load(f)
+        if len(data['features']) != 0:
+            with open('log.txt', 'a') as f:
+                for eq_data in data['features']:
+                    log_str = f'{time.strftime("Date : %Y-%m-%d | Time : %H:%M:%S", current_time)} | {eq_data['properties']['title']} | Coo : {eq_data['geometry']['coordinates']}\n'
+                    f.write(log_str)
+                    global max_mag, max_mag_loc, eq_coo
+                    if eq_data['properties']['mag'] > max_mag:
+                        max_mag = eq_data['properties']['mag']
+                        max_mag_loc = eq_data['properties']['title']
+                        eq_coo = eq_data['geometry']['coordinates']
+            dist = math.sqrt(math.pow((eq_coo[0]-lon), 2)+math.pow((eq_coo[1]-lat), 2))
+            if max_mag > 5.5:
+                seis_label['text'] = f'!!! {max_mag_loc} !!!'
+                seis_label['fg'] = 'red'
+                mag_bar['style'] = "red.Horizontal.TProgressbar"
+                mag_bar['value'] = max_mag*10
+                dist_bar['value'] = 100-((dist/1750)*100)
+                root.update_idletasks()
+                eas()
+            else:
+                seis_label['text'] = f'!!! {max_mag_loc} !!!'
+                mag_bar['style'] = "green.Horizontal.TProgressbar"
+                mag_bar['value'] = max_mag*10
+                dist_bar['value'] = 100-((dist/1750)*100)
     else:
         print("Something went wrong with usgs.")
         exit(1)
@@ -89,8 +127,28 @@ if ip_permission:
         progress_bar = ttk.Progressbar(progress_bar_frame, style="green.Horizontal.TProgressbar", length=600, mode='determinate')
         progress_bar.grid(row=1, column=0, padx=10, pady=10)
 
+        seis_label = Label(frm, anchor='center', text="No seismic activity detected.", bg="#00FFFF", font=("Courier New", 15, "bold"))
+        seis_label.grid(row=4, column=0, padx=10, pady=10)
+
+        mag_bar_frame = Frame(frm, bg="#00FFFF")
+        mag_bar_frame.grid(row=5, column=0, padx=10, pady= 10)
+
+        
+
+        mag_label = Label(mag_bar_frame, text="Magnitude : ", bg="#00FFFF", font=("Courier New", 15, "bold"))
+        mag_label.grid(row=0, column=0, padx=10, pady=10)
+
+        mag_bar = ttk.Progressbar(mag_bar_frame, style="green.Horizontal.TProgressbar", length=400, mode='determinate')
+        mag_bar.grid(row=0, column=1, padx=10, pady=10, sticky='w')
+
+        dist_label = Label(mag_bar_frame, text="Distance : ", bg="#00FFFF", font=("Courier New", 15, "bold"))
+        dist_label.grid(row=1, column=0, padx=10, pady=10)
+
+        dist_bar = ttk.Progressbar(mag_bar_frame, style="green.Horizontal.TProgressbar", length=400, mode='determinate')
+        dist_bar.grid(row=1, column=1, padx=10, pady=10, sticky='w')
+
         update_loop()
-        button0 = ttk.Button(frm, text="Quit", command=root.destroy).grid(row=4, column=0, pady=25)
+        button0 = ttk.Button(frm, text="Quit", command=root.destroy).grid(row=6, column=0, pady=25)
         root.mainloop()
     else:
         print("Something went wrong with ipinfo.")
